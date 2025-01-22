d0b5f64a16b2c4facd68f5bee5b297df7309107b
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2025-01-22 18:10:49 +0100

    Setup the repo by creating a README, running the init command and setup the overall dir structure for the project, insert a basic express appliccation setup in the startpoint

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # book_lending
 A 3-tiered web application where a user can lend books (not connected to any booking system). Created using JS, NodeJS, EJS and express for the front- and back- end. MySQL for the DB.
+
+This is an assignment in the course 1DV503 Database technology at the Linnaeus University in Sweden.
+
+## Assignment description
+
+This is a web application that is run on the backend, rendering the front-end from the backend through routing. It should be built using NodeJS and a frontend framework (I'm using EJS). The main focus on the assignment is not the frontend though, but the backend, in particular the database and working with SQL through a DBMS, my database will be built using a MySQL DB.
+
+A user should be able to sign up, login/logout, browse books and subsequently "lend" the books onto their account.
+
+| Tech | Use area |
+|------|----------|
+| JavaScript | Programming language |
+| NodeJS | Main application framework |
+| Express | Server base |
+| EJS | Frontend framework |
+| MySQL | DataBase |
+
+## Tasks
+
+- Create schemas for the DB according to the given relational model and populate the DB with the provided SQL file.
+
+- Start building the base for the express server using routing.
+
+- Implement basic reg, login and logout features (session cookies?)
+
+- Implement browsing books by subject.
+
+- Implement searching books by Author/Title
+
+- Implement a check out system for the books.
+
+- Implement a way to quit the application. (The code should not crash during wrong user input but should instead show a message or do nothing)
---
+++ src/server.js
@@ -0,0 +1,115 @@
+/**
+ * @file Defines the main application.
+ * @module server
+ * @author Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
+ */
+
+import express from 'express'
+import expressLayouts from 'express-ejs-layouts'
+import session from 'express-session'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { connectToDatabase } from './config/' // TO_DO: Insert DB connect file.
+import { sessionOptions } from './config/sessionOptions.js'
+import { router } from './routes/router.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+try {
+  // Connect to DB.
+  // await connectToDatabase()
+
+  // Creates an Express application.
+  const app = express()
+
+  // Get the directory name of this module's path.
+  const directoryFullName = dirname(fileURLToPath(import.meta.url))
+
+  // Set the base URL to use for all relative URLs in a document.
+  const baseURL = process.env.BASE_URL || '/'
+
+  // View engine setup.
+  app.set('view engine', 'ejs')
+  app.set('views', join(directoryFullName, 'views'))
+  app.set('layout', join(directoryFullName, 'views', 'layouts', 'default'))
+  app.set('layout extractScripts', true)
+  app.set('layout extractStyles', true)
+  app.use(expressLayouts)
+
+  // Parse requests of the content type application/x-www-form-urlencoded.
+  // Populates the request object with a body object (req.body).
+  app.use(express.urlencoded({ extended: false }))
+
+  // Serve static files.
+  app.use(express.static(join(directoryFullName, '..', 'public')))
+
+  // Setup and use session middleware (https://github.com/expressjs/session)
+  if (process.env.NODE_ENV === 'development') {
+    app.set('trust proxy', 1) // trust first proxy
+  }
+
+  app.use(session(sessionOptions))
+
+  // Middleware to be executed before the routes.
+  app.use((req, res, next) => {
+    // Flash messages - survives only a round trip.
+    res.locals.user = req.session.user
+
+    // If the account is deleted, terminate the session. If not save the flash message and move on.
+    if (req.session.flash && req.session.flash.text.includes('The account was deleted successfully.')) {
+      res.locals.flash = req.session.flash
+      req.session.destroy()
+    } else if (req.session.flash) {
+      res.locals.flash = req.session.flash
+      delete req.session.flash
+    }
+
+    // Pass the base URL to the views.
+    res.locals.baseURL = baseURL
+
+    next()
+  })
+
+  // Register routes.
+  app.use('/', router)
+
+  // Error handler.
+  app.use((err, req, res, next) => {
+    console.error(err)
+
+    // 404 Not Found.
+    if (err.status === 404) {
+      res
+        .status(404)
+        .sendFile(join(directoryFullName, 'views', 'errors', '404.html'))
+      return
+    }
+
+    // 500 Internal Server Error (in production, all other errors send this response).
+    if (process.env.NODE_ENV === 'production') {
+      res
+        .status(500)
+        .sendFile(join(directoryFullName, 'views', 'errors', '500.html'))
+      return
+    }
+
+    // ---------------------------------------------------
+    // ⚠️ WARNING: Development Environment Only!
+    //             Detailed error information is provided.
+    // ---------------------------------------------------
+
+    // Render the error page.
+    res
+      .status(err.status || 500)
+      .render('errors/error', { error: err })
+  })
+
+  // Starts the HTTP server listening for connections.
+  const server = app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${server.address().port}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}
